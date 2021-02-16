872b1a05106dd612802b434642c4eca094265435
margeschrec <margeschrec@gmail.com>
2021-02-15 20:17:12 -0500

    commit monday night before branch move

--- contact.php
+++ contact.php
@@ -3,9 +3,28 @@
     <title>Contact Bar-Bill</title>
 </head>
 <body>
+<<<<<<< HEAD
 <h1>Contact Us</h1>
 <h2>We will get back to you as soon as possible.</h2>
 <div id=contactform>
+=======
+<header>
+        <img src="images/bbwhite_logo.png" id="logo" alt="bar bill logo" width="10%">
+        <h1>bar-bill tavern: East Aurora and Clarence, NY</h1><br />
+        <nav>
+            <a href="home.html" class="navlink">Home</a>
+            <a href="menu.html" class="navlink">Menu</a>
+            <a href="takeout.html" class="navlink">Take Out</a>
+            <a href="aboutus.html" class="navlink">About Us</a>
+            <a href="FAQ.html" class="navlink">FAQ</a>
+            <a href="contact.php" class="navlink">Contact Us</a>
+        </nav>
+    </header>
+</br>
+    <img src="http://www.buffaloeats.org/wp-content/uploads/2014/09/barbill4-1024x682.jpg" id="contactphoto" width="80%">
+    <h2 id="contactpgheader">Contact Us </br> We will get back to you as soon as possible.</h2>
+    <div class="contactform">
+>>>>>>> 53c5c57 (commit monday night before branch move)
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
         Name: <input type="text" name="name"></br>
         E-mail: <input type="text" name="email"></br>
@@ -17,8 +36,8 @@
                 <input type="file" id="myfile" name="myfile"></br>
                 <input type="submit" name="submit" value="Submit">
     </form> 
-</div>
-</div id=php>
+    </div>
+<   </div id=php>
     <?php
     if(isset($_POST['submit'])){
         $name = "";
@@ -55,6 +74,10 @@
         }   
     }
     ?>
+<<<<<<< HEAD
 
+=======
+    </div>
+>>>>>>> 53c5c57 (commit monday night before branch move)
 </body>
 </html>
