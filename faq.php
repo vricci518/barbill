0e099bb3d63886d5e13966c5be5fff1fb94d05d3
margeschrec <margeschrec@gmail.com>
2021-02-17 19:11:51 -0500

    replaced and reworked css files, changed to php, worked on FAQ

--- faq.php
+++ faq.php
@@ -2,7 +2,8 @@
 <html>
     <head>
         <title>FAQ</title>
-        <link rel="stylesheet" href="barbill.css">
+        <link rel="stylesheet" href="css\barbill.css">
+        <link rel="stylesheet" href="css\faq.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">   
     </head>
 <body>
@@ -10,17 +11,18 @@
         <img src="images/bbwhite_logo.png" id="logo" alt="bar bill logo" width="10%">
         <h1>bar-bill tavern: East Aurora and Clarence, NY</h1><br />
         <nav>
-            <a href="home.html" class="navlink">Home</a>
-            <a href="menu.html" class="navlink">Menu</a>
-            <a href="socialmedia.html" class="navlink">Social Media</a>
-            <a href="aboutus.html" class="navlink">About Us</a>
-            <a href="FAQ.html" class="navlink">FAQ</a>
+        <a href="home.php" class="navlink">Home</a>
+            <a href="menu.php" class="navlink">Menu</a>
+            <a href="socialmedia.php" class="navlink">Social Media</a>
+            <a href="aboutus.php" class="navlink">About Us</a>
+            <a href="faq.php" class="navlink">FAQ</a>
             <a href="contact.php" class="navlink">Contact Us</a>
         </nav>
     </header>
-<div class="FAQ">
+<div id="FAQtitle">
 <h2>Frequently Asked Questions</h2>
-<div>
+</div>
+    <div class="FAQ">
     <h2>What are your Hours of Operation?</h2>
     <ul>
         <li>East Aurora dine-in is open monday - sunday from 11:30 am til 10:00 pm (last seatings at 8:45 pm)</li>
@@ -30,26 +32,30 @@
         <li>Please Note: for online orders once the kitchen cook time exceeds the kitchen close time the online ordering form automatically shuts off, sometimes making customers unable to place an online order during our normal open hours. This only happens when our kitchen has hit our capacity for the night- we apologize for any inconvenience.</li>  
       </ul>
     </ul>
+    </div>
 </br>
+    <div class="FAQ">
     <h2>What safety measures are you taking for dining at your restaurant?</h2>
     <ul>
         <li>Personal Protective Equipment
-        </br>-all staff must wear a mask
-        </br>-all guests must wear a mask unless seated at the table</li>
+            -All staff must wear a mask
+            -All guests must wear a mask unless seated at the table</li>
         <li>Social Distancing
-        </br>-operating at half capacity with extra space between tables
-        </br>-booking by reservation only to enforce limited diners
-        </br>-distancing maintained in common areas
-        </br>-diners wait in their cars until their tables are ready </li>
+            -Operating at half capacity with extra space between tables
+            -Booking by reservation only to enforce limited diners
+            -Distancing maintained in common areas
+            -Diners wait in their cars until their tables are ready </li>
         <li>Cleaning & Sanitizing
-        </br>-surfaces sanitized after every meal
-        </br>-QR codes provided at tables to minimize paper menus
-        </br>-common areas deep cleaned daily</li>
+            -Surfaces sanitized after every meal
+            -QR codes provided at tables to minimize paper menus
+            -Common areas deep cleaned daily</li>
     </ul>
+    </div>
     <div class="FAQimg">
         <img src="https://popmenucloud.com/cdn-cgi/image/width=768,height=768,fit=scale-down,format=auto,quality=60/laworbdj/1f05d53f-5261-4d37-a345-54ec307ad985.jpg"/>
     </div>
 </br>
+    <div class="FAQ">
     <h2>I need to make a change to my reservation- what do I do?</h2>
     <ul>
         <li>If you are unable to make a change to your reservation through your opentable account please give us a call, leave us a message & someone will get back to you as soon as we can.</li>
@@ -62,60 +68,76 @@
         <li>Please note that weekends are usually busy often with longer wait times.</li>
         <li>A cell phone number will be needed at checkout so that we can text you when your order is ready. Please do not input a landline number with your order.</li>
     </ul>
+    </div>
     <div class="FAQimg">
         <img src="https://popmenucloud.com/cdn-cgi/image/width=768,height=768,fit=scale-down,format=auto,quality=60/laworbdj/5a120b00-9b62-42e0-bfc4-b95f52047547.jpg"/>
     </div>
 </br>
+    <div class="FAQ">
     <h2>Can I schedule an order for a later time?</h2>
     <ul>
         <li>So sorry, but our online system is unable to accept pre-orders (same goes for our phone orders). We can only accept takeout orders in real time as they are placed.</li>
     </ul>
+    </div>
     <div class="FAQimg">
         <img src="https://popmenucloud.com/cdn-cgi/image/width=768,height=768,fit=scale-down,format=auto,quality=60/laworbdj/15a1bbc4-9090-4bd6-8e4a-c463f94635f8.png"/>
     </div>
     </br>
+    <div class="FAQ">
     <h2>My giftcard isn't working online - how do i fix this?</h2>
     <ul>
         <li>We've recently switched over our online operating system at all of our locations. If you purchased or received a red giftcard (pictured below) from one of our locations please input your card info into the link below to get a new number that is compatible with our online system.</li>
         <li>If you have any further gift card issues please send us a message through our 'contact us' form or send us a private message on facebook or instagram and someone will get back to you shortly!</li></br>
         <a href="https://www.toasttab.com/bar-bill/findcard" class="button" target="_blank"  >www.toasttab.com/bar-bill/findcard</a> </br>
     </ul>
+    </div>
     <div class="FAQimg">
         <img src="https://popmenucloud.com/cdn-cgi/image/width=768,height=768,fit=scale-down,format=auto,quality=60/laworbdj/da7a6576-6d1f-4f7e-ae5e-b9d10ac413ef.JPG"/>
     </div>
-</br>    
+</br>   
+    <div class="FAQ">
     <h2>I'm trying to place an order during your open hours, but it says online ordering is currently not available. Why can't i place an order?</h2>
     <ul>
         <li>Once the kitchen cook time exceeds the kitchen close time the online ordering form automatically shuts off, sometimes making customers unable to place an online order during our normal open hours. This only happens when our kitchen has hit our capacity for the night- we apologize for any inconvenience.</li>
     </ul>
+    </div>
     <div class="FAQimg">
         <img src="https://popmenucloud.com/cdn-cgi/image/width=768,height=768,fit=scale-down,format=auto,quality=60/laworbdj/a49ce4fd-18ae-4a4d-96cb-ea2748b4a577.png"/>
     </div>
     </br>
+    <div class="FAQ">
     <h2>What happens once i've submitted my order?</h2>
     <ul>
         <li>You will be given an estimated time of when your food will be ready. When you arrive, please stay in your vehicle until you receive a text from us letting you know that your order is done. At that time pull up to the takeout cones & someone will bring your food out to you.</li>
     </ul>
+    </div>
     <div class="FAQimg">
         <img src="https://popmenucloud.com/cdn-cgi/image/width=768,height=768,fit=scale-down,format=auto,quality=60/laworbdj/9db7e650-54d6-44a5-b3f6-070ccfb92adf.jpeg"/>
     </div>
     </br>
+    <div class="FAQ">
     <h2>I have an issue - how can I talk to someone on the phone?</h2>
     <ul>
         <li>You can call us and leave a voicemail at either location. We are doing our very best to get back to those that have left us a message within 30 minutes (during our open hours). If a message is left after hours we will be in touch the following day. We appreciate your patience!</li>
     </ul>
+    </div>
 </br>
+    <div class="FAQ">
     <h2>How far in advance can i place a reservation for a table at Bar-Bill Tavern or Bar-Bill North?</h2>
     <ul>
         <li>Guests are able to place a reservation up to 14 days in advance through our website!</li>
     </ul>
+    </div>
 </br>
+    <div class="FAQ">
     <h2>How can I be updated on announcements, changes or news from Bar-Bill?</h2>
     <ul>
         <li>Make sure you are following us on facebook, instagram & twitter for updates on Bar-Bill Tavern & Bar-Bill North!</li>
     </ul>
+    </div>
 </br>
-<h2>Thank you for your continued patronage & support during this time!</h2>
+<div id="FAQtitle">
+    <h2>Thank you for your continued patronage & support during this time!</h2>
 </div>
 </div>
 <footer>
