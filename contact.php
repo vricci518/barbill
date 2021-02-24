5fa1910769a3c5a429bfac9ef75005cc4d3990d1
margeschrec <margeschrec@gmail.com>
2021-02-23 20:18:08 -0500

    added bs navbar, added link paths for menus and new info menu title for about us, contact, faq

--- contact.php
+++ contact.php
@@ -1,24 +1,56 @@
 <!DOCTYPE html>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Contact Bar-Bill</title>
     <link rel="stylesheet" href="css/barbill.css">
-    <link rel="stylesheet" href="css/contact.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+	<link rel="stylesheet" href="css/home.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 
 <body>
     <header>
         <img src="images/bbwhite_logo.png" id="logo" alt="bar bill logo" width="10%">
         <h1>bar-bill tavern: East Aurora and Clarence, NY</h1><br />
-        <nav>
-            <a href="home.php" class="navlink">Home</a>
-            <a href="menu.php" class="navlink">Menu</a>
-            <a href="socialmedia.php" class="navlink">Social Media</a>
-            <a href="aboutus.php" class="navlink">About Us</a>
-            <a href="faq.php" class="navlink">FAQ</a>
-            <a href="contact.php" class="navlink">Contact Us</a>
-        </nav>
+		<nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+			<div class="container-fluid">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="btn btn-primary btn-lg" class="nav-link active" aria-current="page" href="home.php">Home</a>
+						</li>
+						<li class="nav-item dropdown">
+						<a class="btn btn-primary btn-lg" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Menus
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<li><a class="dropdown-item" href="menu.php">menu</a></li>
+								<li><a class="dropdown-item" href="#">menu2</a></li>
+								<li><a class="dropdown-item" href="#">menu3</a></li>
+								<li><a class="dropdown-item" href="#">menu4</a></li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="btn btn-primary btn-lg" class="nav-link" href="socialmedia.php">Social Media</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="btn btn-primary btn-lg" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Info
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<li><a class="dropdown-item" href="aboutus.php">About Us</a></li>
+								<li><a class="dropdown-item" href="faq.php">FAQ</a></li>
+								<li><a class="dropdown-item" href="contact.php">Contact Us</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
     </header>
     <div class="maincontact">
         </br>
@@ -114,6 +146,7 @@
             </div>
         </footer>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 
 </html>
