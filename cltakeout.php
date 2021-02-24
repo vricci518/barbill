4fb7917e97defddf4572fd8e37ab271add057ba6
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-02-24 18:23:28 -0500

    tkt30_added_4_menu_pages_fixed_home_styling

---
+++ cltakeout.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>MENU - Bar-Bill Tavern - Restaurant in NY</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+	<link rel="stylesheet" href="css/barbill.css">
+	<link rel="stylesheet" href="css/menu.css">
+</head>
+
+<body>
+	<header>
+		<img src="images/bbwhite_logo.png" id="logo" alt="bar bill logo">
+		<h1>bar-bill tavern: East Aurora and Clarence, NY</h1><br />
+		<nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+			<div class="container-fluid">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="btn btn-primary btn-lg" class="nav-link active" aria-current="page" href="home.php">Home</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="btn btn-primary btn-lg" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Menus
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<li><a class="dropdown-item" href="eamenu.php">East Aurora Menu</a></li>
+								<li><a class="dropdown-item" href="eatakeout.php">East Aurora Take Out</a></li>
+								<li><a class="dropdown-item" href="clmenu.php">Clarence Menu</a></li>
+								<li><a class="dropdown-item" href="cltakeout.php">Clarence Take Out</a></li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="btn btn-primary btn-lg" class="nav-link" href="socialmedia.php">Social Media</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="btn btn-primary btn-lg" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Info
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<li><a class="dropdown-item" href="aboutus.php">About Us</a></li>
+								<li><a class="dropdown-item" href="faq.php">FAQ</a></li>
+								<li><a class="dropdown-item" href="contact.php">Contact Us</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<div class="mainmenu">
+		<h2 id="menuheader">Clarence Take Out</h2>
+		<div class="menu">
+			<h2>What We're Famous For</h2>
+			<div class="menupic">
+				<img src="images/wingsbeer.jpg" alt="chicken wings">
+			</div>
+			<div class="menutext">
+				<p><b>CHICKEN WINGS</b><br />
+					Mild, Medium, Medium Hot, Hot, Extra Hot, Suicidal, Honey Dijon, Spicy Asian, Cajun (Medium and Hot), Honey Butter BBQ, Hot & Spicy BBQ, or Sicilian.<br />
+					Served with Bar-Bill's own Bleu Cheese dressing made fresh daily. Please specify if you want extra saucy.<br />
+					10 Wings $11.95 • 20 Wings $23.95 • ALL Flats or ALL Drums +$2.50<br />
+					Split Sauce Orders +$1 • Add Cajun to any sauce +$0.50 • Cajun-Medium +$0.50<br />
+					Cajun-Honey Butter BBQ +$0.50 • Sicilian +$1 • Suicidal +$2
+				</p>
+			</div>
+			<div class="menupic">
+				<img src="images/to_eastaurora.jpg" alt="beef on weck">
+			</div>
+			<div class="menutext">
+				<p><b>BEEF ON WECK</b><br />
+					Our specialty - slow roasted & carved while you wait served with Chips & a Pickle (plain roll available)<br />
+					Regular $11.95 • Small $10.95 • Mini $7.95
+				</p>
+			</div>
+			<div class="menupic">
+				<img src="images/chickenfingers.jpg" alt="chicken fingers">
+			</div>
+			<div class="menutext">
+				<p><b>CHICKEN FINGERS</b><br />
+					Tenderloins served with Celery and Blue Cheese Dressing. (All wing flavors available)<br />
+					$10.95
+				</p>
+			</div>
+			<div class="menupic">
+				<img src="images/special.jpg" alt="beef on weck">
+			</div>
+			<div class="menutext">
+				<p><b>BAR-BILL SPECIAL</b><br />
+					Roast Beef, Ham and American or Swiss on toasted Rye with Chips and a Pickle.<br />
+					$11.95
+				</p>
+			</div>
+		</div>
+		<div class="menu">
+			<h2>Starters</h2>
+			<div class="menupic">
+				<img src="images/breadsticks.jpg" alt="chicken wings">
+			</div>
+			<div class="menutext">
+				<p><b>BAR-BILL CHEESY BREADSTICKS</b><br />
+					Breadsticks covered in Bar-Bill's famous Sicilian sauce and melted Mozzarella Cheese. Served with Sicilian sauce and Marinara.<br />
+					$8.95
+				</p>
+			</div>
+			<div class="salad">
+				<p><b>FRESH MOZZARELLA SALAD</b><br />
+					With Tomatoes, Baby Spinach, Red Onions and Creamy Balsamic dressing.<br />
+					$7.95
+				</p>
+			</div>
+			<div class="salad">
+				<p><b>CHEF SALAD</b><br />
+					Spring mix, Tomatoes, Onions, Croutons and Mozzarella Cheese.<br />
+					Choose: Ranch, Honey Mustard, Creamy Balsamic, or Italian
+					$6.95
+				</p>
+			</div>
+		</div>
+		<div class="menu">
+			<h2>Other Good Stuff</h2>
+			<div class="menupic">
+				<img src="images/chickensandwich.jpg" alt="chicken wings">
+			</div>
+			<div class="menutext">
+				<p><b>CHICKEN SANDWICH</b><br />
+					Breaded filet on a Kaiser Roll served with Waffle Fries, Honey Mustard and a Pickle. Add Cheese, Tomato and Onion. (Regular or Buffalo Style)<br />
+					$10.95
+				</p>
+			</div>
+			<div class="menupic">
+				<img src="images/breadedshrimp.jpg" alt="chicken wings">
+			</div>
+			<div class="menutext">
+				<p><b>BREADED SHRIMP</b><br />
+					Butterflied and Fried Shrimp served with Waffle Fries and Sweet Chili dipping sauce.<br />
+					$10.95
+				</p>
+			</div>
+			<div class="menupic">
+				<img src="images/chickenwingpizza.jpg" alt="chicken wings">
+			</div>
+			<div class="menutext">
+				<p><b>BAR-Bill CHICKEN WING PIZZA</b><br />
+					Wing Chicken, Mozzarella Cheese and Spicy Blue Cheese sauce.<br />
+					$10.95
+				</p>
+			</div>
+			<div class="menupic">
+				<img src="images/pepperonipizza.jpg" alt="chicken wings">
+			</div>
+			<div class="menutext">
+				<p><b>PEPPERONI PIZZA</b><br />
+					Cheese and Pepperoni (10").<br />
+					$9.95
+				</p>
+			</div>
+			<div class="menupic">
+				<img src="images/fries.jpg" alt="chicken wings">
+			</div>
+			<div class="menutext">
+				<p><b>FRIES</b><br />
+					All battered and seasoned.<br />
+					Choose: Waffle, Shoe String, Beer Battered, or Sweet Potato
+					$3.95
+				</p>
+			</div>
+		</div>
+	</div>
+	</div>
+	<footer>
+		<div class="footer">
+			<h2>Our Locations</h2>
+			<p><em>East Aurora Takeout</em><br />
+				191 Main Street, Rear<br />
+				East Aurora, NY 14052<br />
+				(716) 652-7959
+			</p>
+			<p><em>East Aurora</em><br />
+				185 Main Street<br />
+				East Aurora, NY 14052<br />
+				(716) 652-7959
+			</p>
+			<p><em>Clarence</em><br />
+				8326 Main Street<br />
+				Clarence, NY 14221<br />
+				(716) 710-9464
+			</p>
+		</div>
+	</footer>
+	</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
