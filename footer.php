70f02528fff0ca5f83b00c79bc3d9e92bb274f2d
margeschrec <margeschrec@gmail.com>
2021-03-04 20:09:22 -0500

    changed look of social media to mirror press page

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 				(716) 652-7959
 			</p>
             </a>
-            <a href="https://www.google.com/maps/search/?api=1&query=191%20Main%20Street%2C%20Rear%2C%20East%20Aurora%2C%20NY%2014052&query_place_id=" target="_blank">
+            <a href="https://www.google.com/maps/place/Bar-Bill+Tavern/@42.7676431,-78.6284672,19z/data=!3m1!4b1!4m5!3m4!1s0x89d3a7d957e7f825:0x6a33d3a817bd1750!8m2!3d42.7676421!4d-78.62792" target="_blank">
 			<p><em>East Aurora</em><br />
 				185 Main Street<br />
 				East Aurora, NY 14052<br />
