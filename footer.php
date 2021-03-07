d5a5ae5aef4bd73625ae3a1fb4bdb11d895cc445
margeschrec <margeschrec@gmail.com>
2021-03-07 14:29:13 -0500

    fixed merge with footer

--- footer.php
+++ footer.php
@@ -23,23 +23,5 @@
 			</p>
             </a>
 		</div>
-		<div class="col-sm-12 col-md-4">
-			<a href="https://www.google.com/maps/place/Bar-Bill+Tavern/@42.767646,-78.6301087,17z/data=!3m1!4b1!4m5!3m4!1s0x89d3a7d957e7f825:0x6a33d3a817bd1750!8m2!3d42.7676421!4d-78.62792" target="_blank">
-				<p><em>East Aurora</em><br />
-					185 Main Street<br />
-					East Aurora, NY 14052<br />
-					(716) 652-7959
-				</p>
-			</a>
-		</div>
-		<div class="col-sm-12 col-md-4">
-			<a href="https://www.google.com/maps/search/?api=1&query=8326%20Main%20Street%2C%20Clarence%2C%20NY%2014221&query_place_id=" target="_blank">
-				<p><em>Clarence</em><br />
-					8326 Main Street<br />
-					Clarence, NY 14221<br />
-					(716) 710-9464
-				</p>
-			</a>
-		</div>
 	</div>
 </footer>
